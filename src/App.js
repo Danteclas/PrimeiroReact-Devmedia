f44afaf5b5ff9e31e6598f2af2b2c72b666b5ebd
Daniel <daniteclas2014@gmail.com>
2022-09-20 14:37:33 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <p>React Js</p>
+      </header>
+      <main>
+        <section className='box-titulo'>
+          <h1>React Js</h1>
+          <p>Biblioteca Javascript para SPAS</p>
+
+          <a href='https://reactjs.org/' target="Site"> Site Oficial</a>
+        </section>
+        <section className='box-requisitos'>
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para aprender React JS:</p>
+
+          <div className='box-cards'>
+            <div className='card'>
+              <h3>HTML</h3>
+              <p>Aprender a estruturar os elementos da página com HTML</p>
+            </div>
+            <div className='card'>
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+            <div className='card'>
+              <h3>JavaScript</h3>
+              <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p className="autor">Curso de introdução de React JS</p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
